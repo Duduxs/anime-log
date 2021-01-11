ec3f7561a51f28620bc6df41565efb027f4297a1
Duduxs <duduxss3@gmail.com>
2021-01-11 08:32:58 -0300

    :rocket: Search-bar
    - Colocando Input com clear para pesquisa
    - Colocando Input de select para selecionar o tipo de pesquisa (User, Anime)

--- front/src/app/components/components.module.ts
+++ front/src/app/components/components.module.ts
@@ -6,9 +6,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
  
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -29,9 +31,11 @@ import { FormComponent } from './form/form.component';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    FormsModule,
     MatButtonModule,
     RouterModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSelectModule
   ],
   exports:[
     HeaderComponent,
--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Type {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  value = "";
+
+  selectedValue: string;
+
+  types: Type[] = [
+    {value: 'user-0', viewValue: 'Usuário'},
+    {value: 'anime-1', viewValue: 'Anime'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
